7492332da5cc0d8bbe0262edbb398631f6fead27
7m3lsh7 <felopater.remon2020@gmail.com>
2025-12-25 16:57:42 +0200

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { ThemeProvider } from "@mui/material";
+import { theme } from "@/src/styles/theme";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      </body>
+    </html>
+  );
+}
