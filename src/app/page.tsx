e87cd2035212d97221be40691c0a63809f5bdeeb
7m3lsh7 <felopater.remon2020@gmail.com>
2025-12-31 01:57:48 +0200

    add a filter  in shaerdCommponents

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,21 @@
 import PrimaryButton from "@/components/shared/PrimaryButton";
 import Button2 from "@/components/shared/Button2";
 import { BUTTON_TEXTS, BUTTON } from "@/data/buttons";
+import Filter from "@/components/shared/FilterComponent";
 
 export default function Home() {
   return (
     <main>
-    <PrimaryButton
-      text={BUTTON_TEXTS.addStudent}
-      onClick={() => console.log("Clicked!")}
-    />
-    
-    <Button2
+      <PrimaryButton
+        text={BUTTON_TEXTS.addStudent}
+        onClick={() => console.log("Clicked!")}
+      />
+
+      <Button2
         text={BUTTON.addStudent}
-      onClick={() => console.log("Clicked!")}
-    />
+        onClick={() => console.log("Clicked!")}
+      />
+      <Filter/>
     </main>
   );
 }
